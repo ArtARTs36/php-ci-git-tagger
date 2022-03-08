712fb4f5935f571cdae59fa867319e8a76ad908e
artarts36 <temicska99@mail.ru>
2022-03-09 00:54:17 +0300

    update readme

--- src/Tagger/GitTagger.php
+++ src/Tagger/GitTagger.php
@@ -6,6 +6,7 @@ use ArtARTs36\CiGitTagger\Contracts\Tagger;
 use ArtARTs36\CiGitTagger\Remote\Credentials;
 use ArtARTs36\CiGitTagger\Tag\Tag;
 use ArtARTs36\GitHandler\Contracts\Handler\GitHandler;
+use ArtARTs36\GitHandler\Exceptions\TagAlreadyExists;
 use ArtARTs36\GitHandler\Making\MakingPush;
 use Psr\Http\Message\UriInterface;
 
@@ -18,14 +19,19 @@ class GitTagger implements Tagger
 
     public function tag(Tag $tag): void
     {
-        $this->git->tags()->add($tag->name, $tag->message?->render());
+        try {
+            $this->git->tags()->add($tag->name, $tag->message?->render());
+        } catch (TagAlreadyExists) {
+            //
+        }
 
         $this->git->pushes()->send(function (MakingPush $push) {
             $push
                 ->onRemote(function (UriInterface $uri) {
                     return $uri->withUserInfo($this->credentials->login, $this->credentials->token);
                 })
-                ->onSetUpStream();
+                ->onSetUpStream()
+                ->tags();
         });
     }
 }
